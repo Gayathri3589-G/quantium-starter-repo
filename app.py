15312b5c22b1402f96499c8dea3d4f9e2dbc7782
Gayathri3589-G <gayu892001@gmail.com>
2026-07-03 11:56:49 +0530

    created dash visualization

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as p
+import plotly.express as px
+from dash import Dash, html, dcc
 
 df1 = p.read_csv("data/daily_sales_data_0.csv")
 df2 = p.read_csv("data/daily_sales_data_1.csv")
@@ -16,3 +18,41 @@ df["price"] = (
 df["sales"] = df["price"]*df["quantity"]
 
 df = df[["sales","region","date"]]
+print(df.head(10))
+
+df["date"] = p.to_datetime(df["date"])
+
+daily_sales = (
+    df.groupby("date")["sales"]
+      .sum()
+      .reset_index()
+      .sort_values("date")
+)
+
+app = Dash()
+fig = px.line(df, x="date", y="sales",title="Pink Morsels sales" ,labels={
+        "date": "Date",
+        "sales": "Sales"
+    })
+
+fig.add_vline(
+    x="2021-01-15",
+    line_dash="dash",
+    annotation_text="Price Increase"
+)
+
+app.layout = html.Div(children=[
+    html.H1(children='Soul Foods Pink Morsel Sales Analysis'),
+
+    html.Div(children='''
+       Sales trend before and after the Pink Morsel price increase.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
